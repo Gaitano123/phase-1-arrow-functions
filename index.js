c48cd6c7b02a1283e3e1431dfa1548312b93a87a
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-14 20:47:24 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    return 2000 / 100;
+}
+// divide (2000, 100);
+
+const square = (parameter) => parameter * parameter;
+square (3)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add (2,3)
